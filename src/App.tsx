513ec8add5063a5c3e5c8841145fdb5bcc4f510d
angela-afonso <angela.afonso.it@gmail.com>
2026-03-02 22:12:41 -0300

    updating link

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/dashboard/cuidador" element={<Dashboard />} />
-          <Route path="/dashboard/paciente" element={<PatientDashboard />} />
+          <Route path="/dashboard/familiar" element={<PatientDashboard />} />
           <Route path="/cadastrar-paciente" element={<RegisterPatient />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
